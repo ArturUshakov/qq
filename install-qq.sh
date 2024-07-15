d43a64351ee8955851ccf52b97e7da976813cd53
Артур Ушаков <a.s.ushakov@efko.ru>
2024-07-15 16:47:43 +0300

    [qq-20240715] Добавлены новые команды и возможности

--- install-qq.sh
+++ install-qq.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
 
 # Функция для вывода цветного текста
 print_colored() {
@@ -12,12 +14,12 @@ if ! command -v docker &> /dev/null; then
 fi
 
 # Цвета для вывода
-GREEN="32"
-BLUE="34"
-YELLOW="33"
 RED="31"
-WHITE="37"
+GREEN="32"
+YELLOW="33"
+BLUE="34"
 CIAN="36"
+WHITE="37"
 
 # Путь к скрипту и файлу версии
 script_dir="$HOME/qq"
@@ -34,232 +36,9 @@ if grep -q "alias qq=" "$alias_file"; then
 fi
 
 # Создание скрипта qq.sh
-cat > "$script_path" << 'EOF'
-#!/bin/bash
+curl -o "$script_path" "https://raw.githubusercontent.com/ArturUshakov/qq/master/qq.sh"
 
-VERSION=0.5.1
-
-# Цвета для вывода
-GREEN=32
-BLUE=34
-YELLOW=33
-RED=31
-WHITE=37
-CIAN=36
-BASIC=""
-
-declare -A commands=(
-    ["-h"]="show_help"
-    ["--help"]="show_help"
-    ["-l"]="list_containers"
-    ["--list"]="list_containers"
-    ["-la"]="list_all_containers"
-    ["--list-all"]="list_all_containers"
-    ["-gph"]="generate_password_hash"
-    ["--generate-password-hash"]="generate_password_hash"
-    ["-i"]="qq_get_info"
-    ["--info"]="qq_get_info"
-)
-
-stdout() {
-    local color=$1
-    shift
-    local message=$*
-    if [ -z "$color" ] || [ "$color" == "$BASIC" ]; then
-        echo "$message"
-    else
-        echo -e "\033[${color}m${message}\033[0m"
-    fi
-}
-
-qq_get_info() {
-    stdout $YELLOW "Скрипт qq"
-    stdout $YELLOW "Версия: $VERSION"
-    stdout $YELLOW "Репозиторий скрипта: https://github.com/ArturUshakov/qq"
-    stdout $YELLOW "По вопросам и предложениям писать: https://t.me/Mariores"
-}
-
-generate_password_hash() {
-    local password=$1
-    local hash=$(php -r "echo password_hash('$password', PASSWORD_DEFAULT);")
-    stdout $WHITE "Hashed password: $hash"
-}
-
-check_version() {
-    local latest_version=$(curl -s https://raw.githubusercontent.com/ArturUshakov/qq/master/version.txt)
-
-    if [ "$latest_version" != "$VERSION" ]; then
-        stdout $RED "\nВНИМАНИЕ!"
-        stdout $WHITE "Доступна новая версия скрипта ($latest_version)"
-        stdout $WHITE "Пожалуйста, обновите скрипт командой:"
-        stdout $CIAN "qq update"
-    fi
-}
-
-update() {
-    curl -s https://raw.githubusercontent.com/ArturUshakov/qq/master/install-qq.sh | bash
-
-    changelog=$(curl -s https://raw.githubusercontent.com/ArturUshakov/qq/master/CHANGELOG.md)
-    version=$(echo "$changelog" | grep -E '^## \[.*\] - ' | head -n 1 | sed -E 's/^## \[([0-9.]+)\].*/\1/')
-    changes=$(echo "$changelog" | awk '/^## \['"$version"'\] - /{flag=1; next} /^## /{flag=0} flag')
-
-    stdout $BASIC "====================================="
-    stdout $GREEN "Скрипт обновлен до версии $version"
-    stdout $YELLOW "Изменения в версии $version:"
-    stdout 36 "$changes"
-    echo "$version" > "$HOME/qq/version.txt"
-    stdout $YELLOW "\nДля обновления скрипта запустите новый терминал или выполните команду:"
-    stdout 36 "source ~/.bashrc"
-}
-
-show_help() {
-    local cmd_descriptions=(
-        "qq -h, --help       Выводит это сообщение"
-        "qq -l, --list       Выводит список запущенных контейнеров"
-        "qq -la, --list-all  Выводит список всех контейнеров"
-        "qq -i, --info       Выводит информацию о скрипте"
-        "qq -gph [password]  Генерирует хэш пароля"
-        "qq [фильтр]         Останавливает все контейнеры, соответствующие фильтру"
-        "qq                  Останавливает все запущенные контейнеры"
-        "qq update           Выполняет обновление qq до актуальной версии"
-    )
-
-    local max_length=0
-    for cmd in "${cmd_descriptions[@]}"; do
-        local length=${#cmd}
-        (( length > max_length )) && max_length=$length
-    done
-
-    local width=$((max_length + 4))
-
-    printf "\e[1;34m%-${width}s\e[0m\n" "ДОСТУПНЫЕ КОМАНДЫ"
-    for cmd in "${cmd_descriptions[@]}"; do
-        printf "\e[1;32m%-${width}s\e[0m\n" "$cmd"
-    done
-}
-
-docker_action() {
-    local action=$1
-    containers=$(docker ps -q)
-    if [ -n "$containers" ]; then
-        mapfile -t container_ids <<< "$containers"
-        mapfile -t names < <(docker inspect --format "{{.Name}}" "${container_ids[@]}" | cut -c2-)
-        for i in "${!container_ids[@]}"; do
-            container=${container_ids[$i]}
-            if [[ -z "$filter" || "${names[$i]}" == *"$filter"* ]]; then
-                docker "$action" "$container" > /dev/null 2>&1 &
-                pids+=($!)
-                container_names["$container"]="${names[$i]}"
-            fi
-        done
-    else
-        echo -e "\e[31m[END]\e[0m Нет запущенных контейнеров для остановки"
-        exit 1
-    fi
-}
-
-check_containers() {
-    local remaining=${#pids[@]}
-    while [ "$remaining" -gt 0 ]; do
-        for i in "${!pids[@]}"; do
-            if [ -n "${pids[$i]}" ] && ! kill -0 "${pids[$i]}" 2>/dev/null; then
-                container_id=${container_ids[$i]}
-                container_name=${container_names["$container_id"]}
-                if [ -n "$container_name" ]; then
-                    echo -e "\e[33m[INFO]\e[0m Остановлен контейнер: \e[36m$container_name\e[0m"
-                else
-                    echo -e "\e[33m[INFO]\e[0m Остановлен контейнер: \e[36m${container_ids[$i]}\e[0m"
-                fi
-                unset pids[$i]
-                ((remaining--))
-            fi
-        done
-        sleep 1
-    done
-    echo -e "\e[32m[INFO]\e[0m Все контейнеры успешно остановлены"
-}
-
-stop_containers() {
-    docker_action "stop"
-}
-
-list_containers() {
-    containers=$(docker ps -q)
-    if [ -n "$containers" ]; then
-        mapfile -t container_ids <<< "$containers"
-        mapfile -t names < <(docker inspect --format "{{.Name}}" "${container_ids[@]}" | cut -c2-)
-        for name in "${names[@]}"; do
-            echo -e "\e[32m[ACTIVE]\e[0m Контейнер: \e[36m${name}\e[0m"
-        done
-    else
-        echo -e "\e[31m[END]\e[0m Нет запущенных контейнеров"
-        echo -e "Для просмотра всех контейнеров выполните \e[33mqq -la\e[0m"
-    fi
-}
-
-list_all_containers() {
-    containers=$(docker ps -a -q)
-    if [ -n "$containers" ]; then
-        mapfile -t container_ids <<< "$containers"
-        mapfile -t names < <(docker inspect --format "{{.Name}}" "${container_ids[@]}" | cut -c2-)
-
-        printf "%-55s\n" "---------------------------------------------------------"
-        printf "| %-40s | %-10s |\n" "Container Name" "Status"
-        printf "%-55s\n" "---------------------------------------------------------"
-
-        for i in "${!container_ids[@]}"; do
-            local status=$(docker inspect --format "{{.State.Status}}" "${container_ids[$i]}")
-            local status_text="STOP"
-            local status_color="\033[31m"  # Red for STOP
-            if [[ "$status" == "running" ]]; then
-                status_text="ACTIVE"
-                status_color="\033[32m"  # Green for ACTIVE
-            fi
-            printf "| %-40s | %b%-10s\033[0m |\n" "${names[$i]}" "$status_color" "$status_text"
-        done
-
-        printf "%-55s\n" "---------------------------------------------------------"
-    else
-        echo -e "\033[31m[ERROR]\033[0m Нет контейнеров"
-    fi
-}
-
-main() {
-    declare -A container_names
-    pids=()
-    start_times=()
-    filter=""
-
-    if [[ "$1" == "update" ]]; then
-        update
-        exit 0
-    elif [[ -n "$1" && -n "${commands[$1]}" ]]; then
-        ${commands[$1]} "$2"
-        check_version
-        exit 0
-    elif [[ -n "$1" ]]; then
-        filter="$1"
-        echo -e "\e[34m[START]\e[0m Начинаем остановку контейнеров с фильтром: \e[36m$filter\e[0m"
-    else
-        echo -e "\e[34m[START]\e[0m Начинаем остановку всех контейнеров..."
-    fi
-
-    stop_containers
-
-    if [ -n "$containers" ]; then
-        check_containers
-    else
-        echo -e "\e[31m[INFO]\e[0m Нет запущенных контейнеров для остановки"
-    fi
-    echo -e "\e[34m[FINISH]\e[0m Процесс завершен"
-    check_version
-}
-
-main "$@"
-
-EOF
-
-chmod +x "$script_path"
+chmod u+x "$script_path"
 
 echo "alias qq='$script_path'" >> "$alias_file"
 
---
+++ qq.sh
@@ -0,0 +1,215 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+VERSION=0.5.2
+
+# Цвета для вывода
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+CIAN="\033[36m"
+WHITE="\033[37m"
+NOANSI="\033[0m"
+
+declare -A commands=(
+    ["-h"]="show_help"
+    ["--help"]="show_help"
+    ["-l"]="list_containers"
+    ["--list"]="list_containers"
+    ["-la"]="list_all_containers"
+    ["--list-all"]="list_all_containers"
+    ["-gph"]="generate_password_hash"
+    ["--generate-password-hash"]="generate_password_hash"
+    ["-i"]="qq_get_info"
+    ["--info"]="qq_get_info"
+    ["--update"]="update"
+)
+
+stdout() {
+    local message=$*
+    echo -e "$message"
+}
+
+qq_get_info() {
+    stdout "$YELLOWСкрипт qq$NOANSI\n$YELLOWВерсия: $VERSION$NOANSI\n$YELLOWРепозиторий скрипта: https://github.com/ArturUshakov/qq$NOANSI\n$YELLOWПо вопросам и предложениям писать: https://t.me/Mariores$NOANSI"
+}
+
+generate_password_hash() {
+    local password=$1
+    local hash=$(php -r "echo password_hash('$password', PASSWORD_DEFAULT);")
+    stdout "$WHITEHashed password: $hash$NOANSI"
+}
+
+check_version() {
+    local latest_version=$(curl -s https://raw.githubusercontent.com/ArturUshakov/qq/master/version.txt)
+    if [ "$latest_version" != "$VERSION" ]; then
+        stdout "$RED\nВНИМАНИЕ!$NOANSI\n$WHITEДоступна новая версия скрипта ($latest_version)$NOANSI\n$WHITEПожалуйста, обновите скрипт командой:$NOANSI\n$CIANqq --update$NOANSI"
+    fi
+}
+
+update() {
+    curl -s https://raw.githubusercontent.com/ArturUshakov/qq/master/install-qq.sh | bash
+    local changelog=$(curl -s https://raw.githubusercontent.com/ArturUshakov/qq/master/CHANGELOG.md)
+    local version=$(echo "$changelog" | grep -E '^## \[.*\] - ' | head -n 1 | sed -E 's/^## \[([0-9.]+)\].*/\1/')
+    local changes=$(echo "$changelog" | awk '/^## \['"$version"'\] - /{flag=1; next} /^## /{flag=0} flag')
+    stdout "====================================="
+    stdout "$GREENСкрипт обновлен$NOANSI до версии $RED$version$NOANSI"
+    stdout "$YELLOWИзменения в версии $version:$NOANSI"
+    stdout "$CIAN$changes$NOANSI"
+    echo "$version" > "$HOME/qq/version.txt"
+    stdout "$YELLOW\nДля обновления скрипта запустите новый терминал или выполните команду:$NOANSI\n$CIANsource ~/.bashrc$NOANSI"
+}
+
+show_help() {
+    local cmd_descriptions=(
+        "qq -h, --help       Выводит это сообщение"
+        "qq -l, --list       Выводит список запущенных контейнеров"
+        "qq -la, --list-all  Выводит список всех контейнеров"
+        "qq -i, --info       Выводит информацию о скрипте"
+        "qq -gph [password]  Генерирует хэш пароля"
+        "qq [фильтр]         Останавливает все контейнеры, соответствующие фильтру"
+        "qq                  Останавливает все запущенные контейнеры"
+        "qq --update         Выполняет обновление qq до актуальной версии"
+    )
+
+    local max_length=0
+    for cmd in "${cmd_descriptions[@]}"; do
+        local length=${#cmd}
+        (( length > max_length )) && max_length=$length
+    done
+
+    local width=$((max_length + 4))
+
+    printf "\e[1;34m%-${width}s\e[0m\n" "ДОСТУПНЫЕ КОМАНДЫ"
+    for cmd in "${cmd_descriptions[@]}"; do
+        printf "\e[1;32m%-${width}s\e[0m\n" "$cmd"
+    done
+}
+
+docker_action() {
+    local action=$1
+    local containers=$(docker ps -q)
+    if [ -n "$containers" ]; then
+        mapfile -t container_ids <<< "$containers"
+        mapfile -t names < <(docker inspect --format "{{.Name}}" "${container_ids[@]}" | cut -c2-)
+        for i in "${!container_ids[@]}"; do
+            local container=${container_ids[$i]}
+            if [[ -z "$filter" || "${names[$i]}" == *"$filter"* ]]; then
+                docker "$action" "$container" > /dev/null 2>&1 &
+                pids+=($!)
+                container_names["$container"]="${names[$i]}"
+            fi
+        done
+    else
+        stdout "$RED[END] Нет запущенных контейнеров для остановки$NOANSI"
+        exit 1
+    fi
+}
+
+check_containers() {
+    local remaining=${#pids[@]}
+    while [ "$remaining" -gt 0 ]; do
+        for i in "${!pids[@]}"; do
+            if [ -n "${pids[$i]}" ] && ! kill -0 "${pids[$i]}" 2>/dev/null; then
+                local container_id=${container_ids[$i]}
+                local container_name=${container_names["$container_id"]}
+                if [ -n "$container_name" ]; then
+                    stdout "$YELLOW[INFO] Остановлен контейнер: $CIAN$container_name$NOANSI"
+                else
+                    stdout "$YELLOW[INFO] Остановлен контейнер: $CIAN${container_ids[$i]}$NOANSI"
+                fi
+                unset pids[$i]
+                ((remaining--))
+            fi
+        done
+        sleep 1
+    done
+    stdout "$GREEN[INFO] Все контейнеры успешно остановлены$NOANSI"
+}
+
+stop_containers() {
+    docker_action "stop"
+}
+
+list_containers() {
+    local containers=$(docker ps -q)
+    if [ -n "$containers" ]; then
+        mapfile -t container_ids <<< "$containers"
+        mapfile -t names < <(docker inspect --format "{{.Name}}" "${container_ids[@]}" | cut -c2-)
+        for name in "${names[@]}"; do
+            stdout "$GREEN[ACTIVE] Контейнер: $CIAN${name}$NOANSI"
+        done
+    else
+        stdout "$RED[END] Нет запущенных контейнеров$NOANSI"
+        stdout "Для просмотра всех контейнеров выполните $YELLOWqq -la$NOANSI"
+    fi
+}
+
+list_all_containers() {
+    local containers=$(docker ps -a -q)
+    if [ -n "$containers" ]; then
+        mapfile -t container_ids <<< "$containers"
+        mapfile -t names < <(docker inspect --format "{{.Name}}" "${container_ids[@]}" | cut -c2-)
+
+        printf "%-55s\n" "---------------------------------------------------------"
+        printf "| %-40s | %-10s |\n" "Container Name" "Status"
+        printf "%-55s\n" "---------------------------------------------------------"
+
+        for i in "${!container_ids[@]}"; do
+            local status=$(docker inspect --format "{{.State.Status}}" "${container_ids[$i]}")
+            local status_text="STOP"
+            local status_color="\033[31m"  # Red for STOP
+            if [[ "$status" == "running" ]]; then
+                status_text="ACTIVE"
+                status_color="\033[32m"  # Green for ACTIVE
+            fi
+            printf "| %-40s | %b%-10s\033[0m |\n" "${names[$i]}" "$status_color" "$status_text"
+        done
+
+        printf "%-55s\n" "---------------------------------------------------------"
+    else
+        stdout "$RED[ERROR] Нет контейнеров$NOANSI"
+    fi
+}
+
+_qq_completions() {
+    local curr_arg="${COMP_WORDS[COMP_CWORD]}"
+    local opts="-h --help -l --list -la --list-all -gph --generate-password-hash -i --info --update"
+    COMPREPLY=( $(compgen -W "${opts}" -- ${curr_arg}) )
+}
+
+complete -F _qq_completions qq
+
+main() {
+    declare -A container_names
+    pids=()
+    filter=""
+
+    if [[ "$1" == "--update" ]]; then
+        update
+        exit 0
+    elif [[ -n "$1" && -n "${commands[$1]}" ]]; then
+        ${commands[$1]} "$2"
+        check_version
+        exit 0
+    elif [[ -n "$1" ]]; then
+        filter="$1"
+        stdout "$BLUE[START] Начинаем остановку контейнеров с фильтром: $CIAN$filter$NOANSI"
+    else
+        stdout "$BLUE[START] Начинаем остановку всех контейнеров...$NOANSI"
+    fi
+
+    stop_containers
+
+    if [ -n "$containers" ]; then
+        check_containers
+    else
+        stdout "$RED[INFO] Нет запущенных контейнеров для остановки$NOANSI"
+    fi
+    stdout "$BLUE[FINISH] Процесс завершен$NOANSI"
+    check_version
+}
+
+main "$@"
