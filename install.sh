b0e8f248f4688cc75372134e0704b035b34f33e1
Артур Ушаков <a.s.ushakov@efko.ru>
2024-08-01 23:36:58 +0300

    [QQ-20240801] новый способ установки

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 INSTALL_DIR="$HOME/qq"
-REPO_URL="https://raw.githubusercontent.com/ArturUshakov/qq/master"
-FILES=("qq.sh" "qq_completions.sh" "CHANGELOG.md" "commands.sh")
+TEMP_DIR="$HOME/qq_temp"
+REPO_URL="https://github.com/ArturUshakov/qq/archive/refs/heads/master.zip"
 QQ_SCRIPT="$INSTALL_DIR/qq.sh"
 COMPLETION_SCRIPT="$INSTALL_DIR/qq_completions.sh"
 ALIAS_CMD="alias qq='$QQ_SCRIPT'"
@@ -21,27 +21,52 @@ print_colored() {
   echo -e "\e[${color_code}m$*\e[0m"
 }
 
-download_file() {
-  curl -s "$REPO_URL/$1" -o "$INSTALL_DIR/$1"
-  if [ $? -eq 0 ]; then
-    print_colored green "$1 загружен успешно."
-    chmod +rx "$INSTALL_DIR/$1"
-  else
-    print_colored red "Ошибка загрузки $1."
-  fi
-}
-
 update_file() {
   grep -q "$2" "$1" || echo "$2" >>"$1"
 }
 
+# Скачивание и распаковка архива
 mkdir -p "$INSTALL_DIR"
-print_colored blue "Загрузка необходимых файлов из GitHub..."
+mkdir -p "$TEMP_DIR"
 
-for file in "${FILES[@]}"; do
-  download_file "$file"
-done
+print_colored blue "Загрузка архива из GitHub..."
+curl -L -o "$TEMP_DIR/master.zip" "$REPO_URL"
+if [ $? -eq 0 ]; then
+  print_colored green "Архив загружен успешно."
+else
+  print_colored red "Ошибка загрузки архива."
+  exit 1
+fi
 
+print_colored blue "Распаковка архива..."
+unzip -q "$TEMP_DIR/master.zip" -d "$TEMP_DIR"
+if [ $? -eq 0 ]; then
+  print_colored green "Архив распакован успешно."
+else
+  print_colored red "Ошибка распаковки архива."
+  exit 1
+fi
+
+print_colored blue "Копирование файлов..."
+cp -r "$TEMP_DIR/qq-master/." "$INSTALL_DIR"
+if [ $? -eq 0 ]; then
+  print_colored green "Файлы скопированы успешно."
+  chmod +rx "$INSTALL_DIR"/*
+else
+  print_colored red "Ошибка копирования файлов."
+  exit 1
+fi
+
+print_colored blue "Удаление ненужных файлов..."
+find "$INSTALL_DIR" -mindepth 1 -maxdepth 1 ! -name 'src' ! -name 'CHANGELOG.md' ! -name 'commands.sh' ! -name 'qq.config' ! -name 'qq.sh' ! -name 'qq_completions.sh' -exec rm -rf {} +
+if [ $? -eq 0 ]; then
+  print_colored green "Ненужные файлы удалены успешно."
+else
+  print_colored red "Ошибка удаления ненужных файлов."
+  exit 1
+fi
+
+# Обновление алиасов
 if [ -f "$HOME/.bash_aliases" ]; then
   ALIAS_FILE="$HOME/.bash_aliases"
 else
@@ -53,5 +78,8 @@ update_file "$ALIAS_FILE" "$COMPLETION_CMD"
 
 source "$HOME/.bashrc"
 [ -f "$HOME/.bash_aliases" ] && source "$HOME/.bash_aliases"
+
+# Очистка временной директории
+rm -rf "$TEMP_DIR"
 
 print_colored green "Установка завершена."
